e7d01919496149939d4092da0dad7418ed1288f6
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-10-12 15:09:49 -0700

    make case-insensitive and reduce size of json

--- services/config.rb
+++ services/config.rb
@@ -29,6 +29,7 @@ required_tags = [
     ${AUDIT_AWS_EC2_TAG_EXAMPLE_EXPECTED_TAGS}
 ];
 logic = ${AUDIT_AWS_EC2_TAG_EXAMPLE_TAG_LOGIC};
+if (logic == "") {logic = "and";}
 ret_alerts = {};
 var BreakException = {};
 
@@ -37,8 +38,11 @@ for (instance_id in json_input) {
     tags = json_input[instance_id]["tags"];
     var tag_names = [];
     for(var i = 0; i < tags.length; i++) {
-        tag_names.push(tags[i]['key'])
         //console.log ("  has tag: " + tags[i]['key']);
+        // implement case-insensitive
+        inst_tag = tags[i]['key'];
+        inst_tag = inst_tag.toLowerCase();
+        tag_names.push(inst_tag)
     }
     num_required = 0;
     num_present = 0;
@@ -46,9 +50,12 @@ for (instance_id in json_input) {
     try {
         for(var i = 0; i < required_tags.length; i++){
             //console.log("    does it have tag " + required_tags[i] + "?");
+
+            // implement case-insensitive
+            required_tag = required_tags[i];
+            required_tag = required_tag.toLowerCase();
             if(tag_names.indexOf(required_tags[i]) == -1) {
                 //console.log("      it does not.");              
-
             } else {
               num_present++;
               //console.log("      it does! num_present is now: " + num_present);
@@ -63,11 +70,13 @@ for (instance_id in json_input) {
           console.log("      instance has enough tags to pass. Need: " + needed + " and it has: " + num_present);          
         } else {
           raw_alert = json_input[instance_id];
-            region = raw_alert["violations"]["ec2-get-all-instances-older-than"]["region"];
-            kill_cmd = "aws ec2 terminate-instances --instance-ids " + instance_id;
-            raw_alert["violations"]["ec2-get-all-instances-older-than"]["kill_script"] = kill_cmd;
-            raw_alert["violations"]["ec2-get-all-instances-older-than"]["aws_console"] = "https://console.aws.amazon.com/ec2/v2/home?region=" + region + "#Instances:search=" + instance_id + ";sort=vpcId";
-            ret_alerts[instance_id] = raw_alert;
+          region = raw_alert["violations"]["ec2-get-all-instances-older-than"]["region"];
+          kill_cmd = "aws ec2 terminate-instances --instance-ids " + instance_id;
+          // leave off the violating_object to reduce size of the json
+          raw_alert["violations"]["ec2-get-all-instances-older-than"]["violating_object"] = {};
+          raw_alert["violations"]["ec2-get-all-instances-older-than"]["kill_script"] = kill_cmd;
+          raw_alert["violations"]["ec2-get-all-instances-older-than"]["aws_console"] = "https://console.aws.amazon.com/ec2/v2/home?region=" + region + "#Instances:search=" + instance_id + ";sort=vpcId";
+          ret_alerts[instance_id] = raw_alert;
           console.log("      instance is in violation: " + instance_id);
         
         }
@@ -89,6 +98,7 @@ required_tags = [
     ${AUDIT_AWS_EC2_TAG_EXAMPLE_EXPECTED_TAGS}
 ];
 logic = ${AUDIT_AWS_EC2_TAG_EXAMPLE_TAG_LOGIC};
+if (logic == "") {logic = "and";}
 ret_alerts = {};
 var BreakException = {};
 kill_all_script = "";
@@ -98,8 +108,11 @@ for (instance_id in json_input) {
     tags = json_input[instance_id]["tags"];
     var tag_names = [];
     for(var i = 0; i < tags.length; i++) {
-        tag_names.push(tags[i]['key'])
         //console.log ("  has tag: " + tags[i]['key']);
+        // implement case-insensitive
+        inst_tag = tags[i]['key'];
+        inst_tag = inst_tag.toLowerCase();
+        tag_names.push(inst_tag)
     }
     num_required = 0;
     num_present = 0;
@@ -107,9 +120,12 @@ for (instance_id in json_input) {
     try {
         for(var i = 0; i < required_tags.length; i++){
             //console.log("    does it have tag " + required_tags[i] + "?");
+
+            // implement case-insensitive
+            required_tag = required_tags[i];
+            required_tag = required_tag.toLowerCase();
             if(tag_names.indexOf(required_tags[i]) == -1) {
                 //console.log("      it does not.");              
-
             } else {
               num_present++;
               //console.log("      it does! num_present is now: " + num_present);
@@ -123,9 +139,9 @@ for (instance_id in json_input) {
         if (num_present >= needed) {
           console.log("      instance has enough tags to pass. Need: " + needed + " and it has: " + num_present);          
         } else {
-            kill_cmd = "aws ec2 terminate-instances --instance-ids " + instance_id;
-            kill_all_script = kill_all_script + kill_cmd + "; ";
-            console.log("      instance is in violation: " + instance_id);
+          kill_cmd = "aws ec2 terminate-instances --instance-ids " + instance_id;
+          kill_all_script = kill_all_script + kill_cmd + "; ";
+          console.log("      instance is in violation: " + instance_id);
         
         }
         throw BreakException;
@@ -164,6 +180,3 @@ coreo_uni_util_notify "advise-ec2-notify-no-tags-older-than-kill-all-script" do
       :to => '${AUDIT_AWS_EC2_TAG_EXAMPLE_ALERT_RECIPIENT}', :subject => 'CloudCoreo ec2 advisor alerts on INSTANCE::stack_name :: INSTANCE::name'
   })
 end
-
-#  "number_of_violations":"STACK::coreo_aws_advisor_ec2.advise-ec2-get-all-instances-older-than.number_violations",
-#  "number_violations_ignored":"STACK::coreo_aws_advisor_ec2.advise-ec2-get-all-instances-older-than.number_ignored_violations",
