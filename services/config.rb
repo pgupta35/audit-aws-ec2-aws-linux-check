b85e3ddaa96233be19df2a2e9a5f8ab8d247291d
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-10-12 12:44:13 -0700

    a

--- services/config.rb
+++ services/config.rb
@@ -130,8 +130,7 @@ for (instance_id in json_input) {
           console.log("      instance has enough tags to pass. Need: " + needed + " and it has: " + num_present);          
         } else {
             kill_cmd = "aws ec2 terminate-instances --instance-ids " + instance_id;
-            kill_all_script = kill_all_script + kill_cmd;
-            kill_all_script.write("\n");
+            kill_all_script = kill_all_script + kill_cmd + "\u000d";
             console.log("      instance is in violation: " + instance_id);
         
         }
