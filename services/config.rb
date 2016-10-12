6030216deb4a8c706ffb4fc225432fbb2a4ccaea
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-10-12 15:31:55 -0700

    another logging cleanup

--- services/config.rb
+++ services/config.rb
@@ -30,7 +30,6 @@ required_tags = [
 ];
 // implement case-insensitive
 required_tags_lower = [];
-console.log(required_tags_lower);
 for (var i = 0; i < required_tags.length; i++) {
   required_tags_lower.push(required_tags[i].toLowerCase());
 };
@@ -104,7 +103,6 @@ required_tags_lower = [];
 for (var i = 0; i < required_tags.length; i++) {
   required_tags_lower.push(required_tags[i].toLowerCase());
 };
-console.log(required_tags_lower);
 logic = ${AUDIT_AWS_EC2_TAG_EXAMPLE_TAG_LOGIC};
 if (logic == "") {logic = "and";}
 ret_alerts = {};
