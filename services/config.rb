2046ebaa8790b8297e508d2437f1000ea489c656
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-12-02 08:19:07 -0800

    conditional jsrunner

--- services/config.rb
+++ services/config.rb
@@ -45,67 +45,83 @@ coreo_aws_advisor_ec2 "advise-ec2-samples-2" do
   regions ["${REGION}"]
 end
 
-# coreo_uni_util_jsrunner "tags-to-notifiers-array-ec2-samples" do
-#   action :run
-#   data_type "json"
-#   packages([
-#                {
-#                    :name => "cloudcoreo-jsrunner-commons",
-#                    :version => "1.1.2"
-#                }       ])
-#   json_input '{ "composite name":"PLAN::stack_name",
-#                 "plan name":"PLAN::name",
-#                 "number_of_checks":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_checks",
-#                 "number_of_violations":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_violations",
-#                 "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_ignored_violations",
-#                 "violations": COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.report}'
-#   function <<-EOH
-# const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
-# const AuditCloudtrail = new CloudCoreoJSRunner(json_input, true, "${AUDIT_AWS_EC2_SAMPLES_ALERT_TO_KILL_RECIPIENT}", "${AUDIT_AWS_EC2_SAMPLES_OWNER_TAG}", 'ec2-samples');
-# const notifiers = AuditCloudtrail.getNotifiers();
-# callback(notifiers);
-#   EOH
-# end
+coreo_uni_util_jsrunner "advisor-output-conditional" do
+  action :run
+  data_type "json"
+  json_input 'COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.report'
+  function <<-EOH
+console.log('advisor-output-conditional is running');
+return_value = "";
+if (null === json_input) {
+  return "{}";
+else {
+  return(json_input);
+}
+callback(return_value);
+  EOH
+end
 
-# Send ec2-samples for email
-# coreo_uni_util_notify "advise-ec2-samples-to-tag-values" do
-#   action :${AUDIT_AWS_EC2_SAMPLES_ALERT_TO_KILL_NOTIF}
-#   notifiers 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-ec2-samples.return'
-# end
+coreo_uni_util_jsrunner "tags-to-notifiers-array-ec2-samples" do
+  action :run
+  data_type "json"
+  packages([
+               {
+                   :name => "cloudcoreo-jsrunner-commons",
+                   :version => "1.1.2"
+               }       ])
+  json_input '{ "composite name":"PLAN::stack_name",
+                "plan name":"PLAN::name",
+                "number_of_checks":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_checks",
+                "number_of_violations":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_violations",
+                "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_ignored_violations",
+                "violations": COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.report}'
+  function <<-EOH
+const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
+const AuditCloudtrail = new CloudCoreoJSRunner(json_input, true, "${AUDIT_AWS_EC2_SAMPLES_ALERT_TO_KILL_RECIPIENT}", "${AUDIT_AWS_EC2_SAMPLES_OWNER_TAG}", 'ec2-samples');
+const notifiers = AuditCloudtrail.getNotifiers();
+callback(notifiers);
+  EOH
+end
 
-# coreo_uni_util_jsrunner "ec2-runner-advise-no-tags-older-than-kill-all-script" do
-#   action :run
-#   data_type "text"
-#   packages([
-#                {
-#                    :name => "cloudcoreo-jsrunner-commons",
-#                    :version => "1.1.2"
-#                }       ])
-#   json_input '{ "composite name":"PLAN::stack_name",
-#                 "plan name":"PLAN::name",
-#                 "number_of_checks":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_checks",
-#                 "number_of_violations":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_violations",
-#                 "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_ignored_violations",
-#                 "violations": COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.report}'
-#   function <<-EOH
-# const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
-# const AuditCloudtrail = new CloudCoreoJSRunner(json_input, true, "${AUDIT_AWS_EC2_SAMPLES_ALERT_TO_KILL_RECIPIENT}", "${AUDIT_AWS_EC2_SAMPLES_OWNER_TAG}", 'ec2-samples');
-# const HTMLKillScripts = AuditCloudtrail.getHTMLKillScripts();
-# callback(HTMLKillScripts)
-#   EOH
-# end
+Send ec2-samples for email
+coreo_uni_util_notify "advise-ec2-samples-to-tag-values" do
+  action :${AUDIT_AWS_EC2_SAMPLES_ALERT_TO_KILL_NOTIF}
+  notifiers 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-ec2-samples.return'
+end
 
-# coreo_uni_util_notify "advise-ec2-notify-no-tags-older-than-kill-all-script" do
-#   action :${AUDIT_AWS_EC2_SAMPLES_ALERT_TO_KILL_NOTIF}
-#   type 'email'
-#   allow_empty ${AUDIT_AWS_EC2_SAMPLES_ALLOW_EMPTY}
-#   send_on "${AUDIT_AWS_EC2_SAMPLES_SEND_ON}"
-#   payload 'COMPOSITE::coreo_uni_util_jsrunner.ec2-runner-advise-no-tags-older-than-kill-all-script.return'
-#   payload_type "html"
-#   endpoint ({
-#       :to => '${AUDIT_AWS_EC2_SAMPLES_ALERT_TO_KILL_RECIPIENT}', :subject => 'Untagged EC2 Instances kill script: PLAN::stack_name :: PLAN::name'
-#   })
-# end
+coreo_uni_util_jsrunner "ec2-runner-advise-no-tags-older-than-kill-all-script" do
+  action :run
+  data_type "text"
+  packages([
+               {
+                   :name => "cloudcoreo-jsrunner-commons",
+                   :version => "1.1.2"
+               }       ])
+  json_input '{ "composite name":"PLAN::stack_name",
+                "plan name":"PLAN::name",
+                "number_of_checks":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_checks",
+                "number_of_violations":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_violations",
+                "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_ignored_violations",
+                "violations": COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.report}'
+  function <<-EOH
+const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
+const AuditCloudtrail = new CloudCoreoJSRunner(json_input, true, "${AUDIT_AWS_EC2_SAMPLES_ALERT_TO_KILL_RECIPIENT}", "${AUDIT_AWS_EC2_SAMPLES_OWNER_TAG}", 'ec2-samples');
+const HTMLKillScripts = AuditCloudtrail.getHTMLKillScripts();
+callback(HTMLKillScripts)
+  EOH
+end
+
+coreo_uni_util_notify "advise-ec2-notify-no-tags-older-than-kill-all-script" do
+  action :${AUDIT_AWS_EC2_SAMPLES_ALERT_TO_KILL_NOTIF}
+  type 'email'
+  allow_empty ${AUDIT_AWS_EC2_SAMPLES_ALLOW_EMPTY}
+  send_on "${AUDIT_AWS_EC2_SAMPLES_SEND_ON}"
+  payload 'COMPOSITE::coreo_uni_util_jsrunner.ec2-runner-advise-no-tags-older-than-kill-all-script.return'
+  payload_type "html"
+  endpoint ({
+      :to => '${AUDIT_AWS_EC2_SAMPLES_ALERT_TO_KILL_RECIPIENT}', :subject => 'Untagged EC2 Instances kill script: PLAN::stack_name :: PLAN::name'
+  })
+end
 
 coreo_uni_util_jsrunner "tags-to-notifiers-array-2" do
   action :run
