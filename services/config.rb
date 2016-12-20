388a06fbac2139e6ed47ded888d96db92c277b4b
123prove123 <123prove123@gmail.com>
2016-12-20 13:09:50 +0600

    update version

--- services/config.rb
+++ services/config.rb
@@ -14,16 +14,15 @@ coreo_aws_advisor_alert "ec2-aws-linux-latest-not" do
   level "Informational"
   objectives ["instances"]
   audit_objects ["reservation_set.instances_set.image_id"]
-  operators ["=~"]
-  alert_when [//]
+  operators ["!="]
+  alert_when ["${AWS_LINUX_AMI}"]
 end
 
 coreo_aws_advisor_ec2 "advise-ec2-samples-2" do
   alerts ["ec2-aws-linux-latest-not"]
   action :advise
-  regions ${AUDIT_AWS_EC2_LINUX_CHECK_REGIONS}
+  regions ["${REGION}"]
 end
-
 
 coreo_uni_util_jsrunner "tags-to-notifiers-array-2" do
   action :run
@@ -31,7 +30,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-2" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.2.3"
+                   :version => "1.1.2"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
                 "plan name":"PLAN::name",
@@ -40,61 +39,9 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-2" do
                 "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples-2.number_ignored_violations",
                 "violations": COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples-2.report}'
   function <<-EOH
-  
-const JSON = json_input;
-const NO_OWNER_EMAIL = "${AUDIT_AWS_EC2_LINUX_CHECK_ALERT_RECIPIENT}";
-const OWNER_TAG = "${AUDIT_AWS_EC2_LINUX_CHECK_OWNER_TAG}";
-const ALLOW_EMPTY = "${AUDIT_AWS_EC2_LINUX_CHECK_ALLOW_EMPTY}";
-const SEND_ON = "${AUDIT_AWS_EC2_LINUX_CHECK_SEND_ON}";
-const AUDIT_NAME = 'ec2-samples';
-
-const ARE_KILL_SCRIPTS_SHOWN = false;
-const EC2_LOGIC = ''; // you can choose 'and' or 'or';
-const EXPECTED_TAGS = ['example_2', 'example_1'];
-
-const WHAT_NEED_TO_SHOWN = {
-    OBJECT_ID: {
-        headerName: 'AWS Object ID',
-        isShown: true,
-    },
-    REGION: {
-        headerName: 'Region',
-        isShown: true,
-    },
-    AWS_CONSOLE: {
-        headerName: 'AWS Console',
-        isShown: true,
-    },
-    TAGS: {
-        headerName: 'Tags',
-        isShown: true,
-    },
-    AMI: {
-        headerName: 'AMI',
-        isShown: false,
-    },
-    KILL_SCRIPTS: {
-        headerName: 'Kill Cmd',
-        isShown: false,
-    }
-};
-
-const VARIABLES = {
-    NO_OWNER_EMAIL,
-    OWNER_TAG,
-    AUDIT_NAME,
-    ARE_KILL_SCRIPTS_SHOWN,
-    EC2_LOGIC,
-    EXPECTED_TAGS,
-    WHAT_NEED_TO_SHOWN,
-    ALLOW_EMPTY,
-    SEND_ON
-};
-
 const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
-const AuditEC2 = new CloudCoreoJSRunner(JSON, VARIABLES);
-const notifiers = AuditEC2.getNotifiers();
-
+const AuditCloudtrail = new CloudCoreoJSRunner(json_input, false, "${AUDIT_AWS_EC2_LINUX_CHECK_RECIPIENT}", "${AUDIT_AWS_EC2_LINUX_CHECK_OWNER_TAG}", 'ec2-samples');
+const notifiers = AuditCloudtrail.getNotifiers();
 callback(notifiers);
   EOH
 end
