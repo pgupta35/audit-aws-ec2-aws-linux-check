81ff0983cdf4edb426efe2f6492e46422da0670a
george-moberly <george-moberly@users.noreply.github.com>
2017-01-04 14:03:08 -0800

    Revert "master to dev"

--- services/config.rb
+++ services/config.rb
@@ -14,14 +14,115 @@ coreo_aws_advisor_alert "ec2-aws-linux-latest-not" do
   level "Informational"
   objectives ["instances"]
   audit_objects ["reservation_set.instances_set.image_id"]
-  operators ["!="]
-  alert_when ["${AWS_LINUX_AMI}"]
+  operators ["=~"]
+  alert_when [//]
 end
 
 coreo_aws_advisor_ec2 "advise-ec2-samples-2" do
   alerts ["ec2-aws-linux-latest-not"]
   action :advise
-  regions ["${REGION}"]
+  regions ${AUDIT_AWS_EC2_LINUX_CHECK_REGIONS}
+end
+
+# the jsrunner will now allow all regions to be specified in the above advisor instead of a single region
+
+# it will also allow the specification of a convention file in the composite to specify violation suppressions
+
+coreo_uni_util_jsrunner "jsrunner-get-not-aws-linux-ami-latest" do
+  action :run
+  provide_composite_access true
+  json_input 'COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples-2.report'
+  packages([
+               {
+                   :name => "js-yaml",
+                   :version => "3.7.0"
+               }       ])
+  function <<-EOH
+    var fs = require('fs');
+    var yaml = require('js-yaml');
+
+// Get document, or throw exception on error
+    try {
+        var properties = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8'));
+        console.log(properties);
+    } catch (e) {
+        console.log(e);
+    }
+
+    var result = {};
+    for (var inputKey in json_input) {
+        var thisKey = inputKey;
+        var ami_id = json_input[thisKey]["violations"]["ec2-aws-linux-latest-not"]["violating_object"]["0"]["object"]["image_id"];
+
+        var cases = properties["variables"]["AWS_LINUX_AMI"]["cases"];
+        var is_violation = true;
+        for (var key in cases) {
+            value = cases[key];
+            console.log(value);
+            if (ami_id === value) {
+                console.log("got a match - this is not a violation");
+                is_violation = false;
+            }
+        }
+        if (is_violation === true) {
+            console.log("no match - this is a violation so copy to result structure");
+            result[thisKey] = json_input[thisKey];
+        }
+    }
+
+    var rtn = result;
+
+    callback(result);
+
+EOH
+end
+
+coreo_uni_util_jsrunner "jsrunner-process-suppressions" do
+  action :run
+  provide_composite_access true
+  json_input 'COMPOSITE::coreo_uni_util_jsrunner.jsrunner-get-not-aws-linux-ami-latest.return'
+  packages([
+               {
+                   :name => "js-yaml",
+                   :version => "3.7.0"
+               }       ])
+  function <<-EOH
+    var fs = require('fs');
+    var yaml = require('js-yaml');
+
+// Get document, or throw exception on error
+    try {
+        var suppressions = yaml.safeLoad(fs.readFileSync('./suppressions.yaml', 'utf8'));
+        console.log(suppressions);
+    } catch (e) {
+        console.log(e);
+    }
+
+    var result = {};
+    for (var inputKey in json_input) {
+        var thisKey = inputKey;
+        var inst_id = inputKey;
+        is_violation = true;
+        for (var suppression in suppressions["suppressions"]["ec2-aws-linux-latest-not"]) {
+            value = suppressions["suppressions"]["ec2-aws-linux-latest-not"][suppression];
+            if (value === inst_id) {
+                console.log("got a match - this violation is suppressed");
+                is_violation = false;
+            }
+
+        }
+        if (is_violation === true) {
+            console.log("no match - this is a violation so copy to result structure");
+            result[thisKey] = json_input[thisKey];
+        }
+    }
+
+    var rtn = result;
+
+    callback(result);
+
+
+EOH
 end
 
 coreo_uni_util_jsrunner "tags-to-notifiers-array-2" do
@@ -30,18 +131,36 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-2" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.1.2"
+                   :version => "1.1.7"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
                 "plan name":"PLAN::name",
                 "number_of_checks":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples-2.number_checks",
                 "number_of_violations":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples-2.number_violations",
                 "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples-2.number_ignored_violations",
-                "violations": COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples-2.report}'
+                "violations": COMPOSITE::coreo_uni_util_jsrunner.jsrunner-process-suppressions.return}'
   function <<-EOH
+  
+const JSON = json_input;
+const NO_OWNER_EMAIL = "${AUDIT_AWS_EC2_LINUX_CHECK_RECIPIENT}";
+const OWNER_TAG = "${AUDIT_AWS_EC2_LINUX_CHECK_OWNER_TAG}";
+const AUDIT_NAME = 'ec2-samples';
+const IS_KILL_SCRIPTS_SHOW = false;
+const EC2_LOGIC = ''; // you can choose 'and' or 'or';
+const EXPECTED_TAGS = [];
+
+const VARIABLES = {
+    'NO_OWNER_EMAIL': NO_OWNER_EMAIL,
+    'OWNER_TAG': OWNER_TAG,
+    'AUDIT_NAME': AUDIT_NAME,
+    'IS_KILL_SCRIPTS_SHOW': IS_KILL_SCRIPTS_SHOW,
+    'EC2_LOGIC': EC2_LOGIC,
+    'EXPECTED_TAGS': EXPECTED_TAGS
+};
+
 const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
-const AuditCloudtrail = new CloudCoreoJSRunner(json_input, false, "${AUDIT_AWS_EC2_LINUX_CHECK_RECIPIENT}", "${AUDIT_AWS_EC2_LINUX_CHECK_OWNER_TAG}", 'ec2-samples');
-const notifiers = AuditCloudtrail.getNotifiers();
+const AuditLinux = new CloudCoreoJSRunner(JSON, VARIABLES);
+const notifiers = AuditLinux.getNotifiers();
 callback(notifiers);
   EOH
 end
