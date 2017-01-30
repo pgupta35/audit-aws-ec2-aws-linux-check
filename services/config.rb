b5eedade00145f39006e3135ed29f288b6f6b9de
George Moberly <george.moberly@gmail.com>
2017-01-30 11:03:57 -0800

    parameterize notifier

--- services/config.rb
+++ services/config.rb
@@ -241,7 +241,7 @@ end
 
 ## Send Notifiers
 coreo_uni_util_notify "advise-ec2-notify-non-current-aws-linux-instance-2" do
-  action :notify
+  action :${AUDIT_AWS_EC2_LINUX_CHECK_HTML_REPORT}
   notifiers 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-2.return'
 end
 
